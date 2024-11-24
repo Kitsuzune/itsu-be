a69cc9183a636cd591dad38ddd45da1e472e8fda
Kitsuzune <teguhwmn189@gmail.com>
2024-11-24 20:52:37 +0700

    switch port

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
 app.use(cors());
@@ -35,6 +35,9 @@ app.use('/uploads/profilePicture', express.static(path.join(__dirname, '/uploads
 app.use('/uploads/productImage', express.static(path.join(__dirname, '/uploads/productImage')));
 app.use('/uploads/bannerImage', express.static(path.join(__dirname, '/uploads/bannerImage')));
 
+app.get('/', (req, res) => {
+    res.send('yee api jalan');
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
